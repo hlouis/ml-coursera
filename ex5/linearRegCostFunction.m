c3af7cae7540083103fcb71a6842e0df12b2e479
Louis Huang <me@hlouis.com>
2016-12-15 15:14:20 +0800

    Cost for liner regression function.

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -19,16 +19,8 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-
-
-
-
-
-
-
-
-
+costs= (X * theta - y) .^ 2;
+J = sum(costs) / (2 * m) + (lambda / (2 * m)) * sum(theta(2:end) .^ 2);
 
 % =========================================================================
 
